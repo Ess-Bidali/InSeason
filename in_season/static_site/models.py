f8a3f71761555c11a2e4913e49ff2e8bf087dbf2
Ess Bidali <essiebidali@gmail.com>
2020-03-31 17:33:11 +0300

    added image to product model

--- in_season/static_site/models.py
+++ in_season/static_site/models.py
@@ -5,11 +5,12 @@ from datetime import datetime
 
 class Product(models.Model):
     name = models.CharField(max_length=25)
-    variety = models.CharField(max_length=25)
-    current_price = models.DecimalField(max_digits=6, decimal_places=2)
+    variety = models.CharField(max_length=25, blank=True, null=True)
+    current_price = models.DecimalField(max_digits=6, decimal_places=2, default=Decimal('0.00'))
     old_price = models.DecimalField(max_digits=6, decimal_places=2, default=Decimal('0.00'))
     units = models.CharField(max_length=25, default='per kg', help_text='eg. per kg, per item')
     available = models.BooleanField(default=False)
+    image = models.ImageField(upload_to='static_site/products/%Y/%m/', blank=True, null=True)
 
     def __str__(self):
         return self.name
