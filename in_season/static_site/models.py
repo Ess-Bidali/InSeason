b09b9222d140970f303ca5ac2978fba362791064
Ess Bidali <essiebidali@gmail.com>
2020-03-31 16:31:37 +0300

    fixed broken home link and added product and order models

--- in_season/static_site/models.py
+++ in_season/static_site/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from decimal import Decimal
+from datetime import datetime
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=25)
+    variety = models.CharField(max_length=25)
+    current_price = models.DecimalField(max_digits=6, decimal_places=2)
+    old_price = models.DecimalField(max_digits=6, decimal_places=2, default=Decimal('0.00'))
+    units = models.CharField(max_length=25, default='per kg', help_text='eg. per kg, per item')
+    available = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    date = models.DateTimeField(default=datetime.now)
--- in_season/static_site/urls.py
+++ in_season/static_site/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'static_site'
+
 urlpatterns = [
     path('', views.index, name='home'),
 ]
