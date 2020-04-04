590c7da55fe9ce174a4ec7c77a454ec8ce64327a
Ess Bidali <essiebidali@gmail.com>
2020-04-04 18:17:06 +0300

    added single product page

--- in_season/static_site/views.py
+++ in_season/static_site/views.py
@@ -12,40 +12,34 @@ def index(request):
 
 
 def shop(request, category=''):
-    if request.GET.get('category'):
-        print('processed')
-        category = request.GET.get('category')
+    category = request.GET.get('category') if request.GET.get('category') else category
+    #if category is provided, perform filter, else grab all products
     if category:
-        return redirect('static_site:filter', category=category)
-    
-    categ = Category.objects.all()
-    products = Product.objects.all()
-    paginator = Paginator(products, 12)
-    if request.GET.get('page'):
-        page = request.GET.get('page')
+        cat = Category.objects.filter(name=category[:-1])[0]
+        products = Product.objects.filter(category=cat)
+        #if request is ajax, return filtered products as a json object
+        if request.is_ajax(): return filter_results(products)            
     else:
-        page = 1
-    products = paginator.get_page(page)
-    filtr = category
-    essentials = {'nbar': 'shop', 'categ': categ, 'products': products, 'filtr':category}
+        products = Product.objects.all()
+
+    categ = Category.objects.all()
+    products = paginate(request, products)
+    filtr = category[:-1]
+    essentials = {'nbar': 'shop', 'categ': categ, 'products': products, 'filtr':filtr}
     return render(request, 'static_site/products.html', essentials )
 
 
-def filter_results(request, category):
-    cat = Category.objects.filter(name=category[:-1])[0]
-    products = Product.objects.filter(category=cat)
-        
-    if request.is_ajax():
-        data = serializers.serialize('json', products)
-        return JsonResponse(data, safe=False)
-    
-    paginator = Paginator(products, 12)
-    if request.GET.get('page'):
-        page = request.GET.get('page')
-    else:
-        page = 1
-    products = paginator.get_page(page)
-    categ = Category.objects.all()
-    filtr = category
-    essentials = {'nbar': 'shop', 'categ': categ, 'products': products, 'filtr': filtr}
-    return render(request, 'static_site/products.html', essentials )
+def filter_results(products):
+    data = serializers.serialize('json', products)
+    return JsonResponse(data, safe=False)
+
+def paginate(request, products):
+    #initialize Paginator object with products and limit of items per page as parameters
+    paginator = Paginator(products, 12) 
+    #get requested page number, else get page 1
+    page = request.GET.get('page') if request.GET.get('page') else 1
+    return paginator.get_page(page)
+
+def single(request):
+    essentials = {'nbar': 'single'}
+    return render(request, 'static_site/single_product.html', essentials)
