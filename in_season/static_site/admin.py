64471b5cb466efd184a36db8a64131f7af599892
Ess Bidali <essiebidali@gmail.com>
2020-04-10 16:04:57 +0300

    added shopping basket page, with functionality to add to basket, remove from basket without login

--- in_season/static_site/admin.py
+++ in_season/static_site/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import *
+from .models import Category, Unit, Currency, Product, Order, OrderItem
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Unit)
+admin.site.register(Currency)
 admin.site.register(Product)
+admin.site.register(OrderItem)
 admin.site.register(Order)
